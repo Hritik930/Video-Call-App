27e65800e93fb0e9ae66c9d6f78834dec6a36a0e
Hritik <hritikblcm@gmail.com>
2024-06-24 23:58:13 +0530

    Api key updated

--- src/component/Constant.jsx
+++ src/component/Constant.jsx
@@ -1,2 +1,2 @@
-export const APP_ID = 144783041; 
-export const SERVER_SECRET = "a414c90c68bad69fb3b7afdc9a983187"
+export const APP_ID = 495049040; 
+export const SERVER_SECRET = "8942590cad39e6c60f17cc66dce55160"
